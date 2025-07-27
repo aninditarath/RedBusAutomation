1501f8ee40ac904be99e33cfea939d4c04b1e1cb
anindita <anindita1193@gmail.com>
2025-07-28 01:17:23 +0530

    28th July

--- src/main/java/org/redbus/redbusautomation.java
+++ src/main/java/org/redbus/redbusautomation.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 /**
  * Hello world!
@@ -20,7 +21,7 @@ public class redbusautomation
 {
     public static void main( String[] args ) throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
+        //options.addArguments("--start-maximized");
 
         WebDriverManager.chromedriver().setup();
         WebDriver wd = new ChromeDriver(options);
@@ -39,13 +40,56 @@ public class redbusautomation
         WebElement fromInput = wait.until(ExpectedConditions.elementToBeClickable(inputField));
         fromInput.sendKeys("Mumbai");
 
-        // Wait for suggestions and click the first one
-        By suggestion = By.xpath("//li[contains(@class,'sc-iwsKbI')]");
-        WebElement firstSuggestion = wait.until(ExpectedConditions.elementToBeClickable(suggestion));
-        firstSuggestion.click();
+//        // Wait for suggestions and click the first one
+//        By suggestion = By.xpath("//li[contains(@class,'sc-iwsKbI')]");
+//        WebElement firstSuggestion = wait.until(ExpectedConditions.elementToBeClickable(suggestion));
+//        firstSuggestion.click();
 
+
+        By Fromserachcatagorylist = By.xpath("//div[contains(@class,'searchCategory')]");
+        List<WebElement> Fromsearchlist= wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(Fromserachcatagorylist,1));
+
+        System.out.println(Fromsearchlist.size());
+
+        WebElement serachtext= Fromsearchlist.get(0);
+
+        By webelementtobeclicked=By.xpath("//div[contains(@class,'listHeader')]");
+        List<WebElement> locations= serachtext.findElements(webelementtobeclicked);
+        System.out.println(locations.size());
+
+        for(WebElement location: locations){
+            if(location.getText().equalsIgnoreCase("Mumbai")){
+                location.click();
+                break;
+            }
+        }
+
+
+        WebElement Toinputbox= wd.switchTo().activeElement();
+        Toinputbox.sendKeys("Pune");
+
+
+        By Toserachcatagorylist = By.xpath("//div[contains(@class,'searchCategory')]");
+        List<WebElement> Tosearchlist= wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(Toserachcatagorylist,1));
+
+        System.out.println(Tosearchlist.size());
+
+        WebElement Toserachtext= Tosearchlist.get(0);
+
+        By TOwebelementtobeclicked=By.xpath("//div[contains(@class,'listHeader')]");
+        List<WebElement> TOlocations= Toserachtext.findElements(TOwebelementtobeclicked);
+        System.out.println(TOlocations.size());
+        for(WebElement location: TOlocations){
+            if(location.getText().equalsIgnoreCase("Pune")){
+                location.click();
+                break;
+            }
+        }
+
+        WebElement clicksearch= wd.findElement(By.xpath("//button[contains(@class,'searchButtonWrapper')]"));
+        clicksearch.click();
         Thread.sleep(2000); // Just for demo pause
-        wd.quit();
+        //wd.quit();
         System.out.println("Test Completed Successfully");
     }
 }
